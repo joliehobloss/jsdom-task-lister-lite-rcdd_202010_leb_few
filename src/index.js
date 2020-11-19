78d337fcfca5520a447847c74d50899428bee46f
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-19 09:26:21 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let subButten=document.getElementById("sub-btn");
+  let description=document.getElementById("new-task-description");
+  let result=document.getElementById("tasks");
+  
+subButten.addEventListener("click",function(event) {
+result.innerHTML += '<li>' + description.value + ' <button onclick="Delete(this);">x</button> </li>';
+
+  event.preventDefault();
 });
+
+
+});
+
+
+     
+//result.innerHTML+=`<li>${description.value}<button>x</button></li>`;
