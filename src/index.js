bf34d064e8590e765b7e51d29ef7716caa320173
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-19 09:46:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,32 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
   let description=document.getElementById("new-task-description");
   let result=document.getElementById("tasks");
   
-form.addEventListener('submit',function(event) {
-result.innerHTML += '<li>' + description.value + ' <button onclick="Delete(this);">x</button> </li>';
- description.value = '';
-  event.preventDefault();
- }, false);
-
-function Delete(currentEl){
-  currentEl.parentNode.parentNode.removeChild(currentEl.parentNode);
-  }
-
+form.addEventListener( 'submit', function( event ) {
+    let text = description.value;
+    if ( text !== '' ) {
+      result.innerHTML += '<li>' + text + ' <button onclick="Delete(this);">Delete</button> </li>';
+      description.value = '';
+      
+    }
+    event.preventDefault();
+  }, false);
 
 });
 
 
-/* var todo = document.querySelector( '#todolist' ),
-      form = document.querySelector( 'form' ),
-      field = document.querySelector( '#newitem' );
-    
-  form.addEventListener( 'submit', function( ev ) {
-    var text = field.value;
-    if ( text !== '' ) {
-      todo.innerHTML += '<li>' + text + ' <button onclick="Delete(this);">Delete</button> </li>';
-      field.value = '';
-      //field.focus();
-    }
-    ev.preventDefault();
-  }, false);*/
 
     
