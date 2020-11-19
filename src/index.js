07a2c8ff3b8ab93880721597c23b4fe56f05d4bd
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-19 09:42:28 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  let form=document.getElementById("sub-btn");
+  let form=document.getElementById("create-task-form");
   let description=document.getElementById("new-task-description");
   let result=document.getElementById("tasks");
   
-subButten.addEventListener("click",function(event) {
+form.addEventListener('submit',function(event) {
 result.innerHTML += '<li>' + description.value + ' <button onclick="Delete(this);">x</button> </li>';
  
   event.preventDefault();
