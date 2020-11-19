6c65f9b5a85b2b49e5617e6709281ced810e7157
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-19 10:02:13 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ document.addEventListener("DOMContentLoaded", () => {
 subButton.addEventListener( 'click', function( event ) {
 result.innerHTML += '<li>' + description.value + ' <button onclick="Delete(this);">x</button> </li>';
     event.preventDefault();
-function Delete(currentEl){
+  });
+  
+  function Delete(currentEl){
   currentEl.parentNode.parentNode.removeChild(currentEl.parentNode);
   }
-  });
 
 });
 
